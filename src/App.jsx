c7bc4d59f10b49a29d8208f4ebe0c5cf1be1f5cd
buddini96 <buddinikl1996@gmail.com>
2025-01-09 12:54:57 +0530

    Added Initial Project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Home/Home'
+import Navbar from './components/Navbar';
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App
